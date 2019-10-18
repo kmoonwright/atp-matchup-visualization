770c6d008935ce6dc2f4e63f785214ac2d1ecfc0
kmoonwright <kmoonwright@outlook.com>
2019-10-18 16:07:02 -0700

    Major refactor and code placement review

--- src/customcircle.js
+++ src/customcircle.js
@@ -1,33 +1,3 @@
-// var diameter = 960,
-//   radius = diameter / 2,
-//   innerRadius = radius - 120;
-
-// var cluster = d3.cluster()
-//   .size([360, innerRadius]);
-
-// var line = d3.radialLine()
-//   .curve(d3.curveBundle.beta(0.85))
-//   .radius(function (d) {
-//     return d.y;
-//   })
-//   .angle(function (d) {
-//     return d.x / 180 * Math.PI;
-//   });
-
-// var svg = d3.select('#custom-circle').append("svg")
-//   .attr("width", diameter)
-//   .attr("height", diameter)
-//   .append("g")
-//   .attr("transform", "translate(" + radius + "," + radius + ")");
-
-// var link = svg.append("g").selectAll(".link"),
-//   node = svg.append("g").selectAll(".node");
-
-
-// D3v5 syntax:
-// d3.csv("./data/2018_matches.csv").then(function (data, error) {
-// D3v4 syntax:
-// let dataUrl = "./dist/data/atp_matches_1969.csv"
 const rawData = {
   1969: "./dist/data/atp_matches_1969.csv",
   1974: "./dist/data/atp_matches_1974.csv",
@@ -40,9 +10,10 @@ const rawData = {
 let link, node;
 
 function runStatRender(year) {
-  
+  // Remove previous data
   d3.select("#custom-circle > svg").remove();
 
+  // SVG element params
   var diameter = 960,
     radius = diameter / 2,
     innerRadius = radius - 120;
@@ -68,7 +39,11 @@ function runStatRender(year) {
   link = svg.append("g").selectAll(".link");
   node = svg.append("g").selectAll(".node");
 
-  
+  // Import, parse, and render data
+  // D3v5 syntax:
+  // d3.csv("./data/2018_matches.csv").then(function (data, error) {
+  // d3.csv(rawData[year]).then(function (data, error) {
+  // D3v4 syntax:
   d3.csv(rawData[year], function (error, data) {
     if (error) throw error;
   
@@ -88,8 +63,7 @@ function runStatRender(year) {
       importData.push(entryObj);
     }
   
-    var root = packageHierarchy2(importData)
-    // .sum(function(d) { return d.size; });
+    var root = packageHierarchy(importData)
   
     cluster(root);
   
@@ -111,7 +85,7 @@ function runStatRender(year) {
       .on("mouseout", mouseouted);
   
     link = link
-      .data(packageImports2(root.leaves()))
+      .data(packageImports(root.leaves()))
       .enter().append("path")
       .each(function (d) {
         d.source = d[0], d.target = d[d.length - 1];
@@ -119,12 +93,11 @@ function runStatRender(year) {
       .attr("class", "link")
       .attr("d", line);
   })
-
 }
 
 
-  // Lazily construct the package hierarchy from class names.
-function packageHierarchy2(classes) {
+  // Construct the package hierarchy from class names.
+function packageHierarchy(classes) {
   var map = {};
 
   function find(name, data) {
@@ -140,16 +113,14 @@ function packageHierarchy2(classes) {
     }
     return node;
   }
-  
   classes.forEach(function(d) {
     find(d.name, d);
   });
-  
   return d3.hierarchy(map[""]);
 }
 
 // Return a list of imports for the given array of nodes.
-function packageImports2(nodes) {
+function packageImports(nodes) {
   var map = {},
       connections = [];
       
@@ -171,31 +142,4 @@ function packageImports2(nodes) {
   });
   
   return connections;
-}
-
-
-// Hover highlighting
-function mouseovered(d) {
-  node
-    .each(function (n) { n.target = n.source = false; });
-
-  link
-    .classed("link--target", function (l) { if (l.target === d) return l.source.source = true; })
-    .classed("link--source", function (l) { if (l.source === d) return l.target.target = true; })
-    .filter(function (l) { return l.target === d || l.source === d; })
-    .raise();
-
-  node
-    .classed("node--target", function (n) { return n.target; })
-    .classed("node--source", function (n) { return n.source; });
-}
-
-function mouseouted(d) {
-  link
-    .classed("link--target", false)
-    .classed("link--source", false);
-
-  node
-    .classed("node--target", false)
-    .classed("node--source", false);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-
+// Modal
 const modalBg = document.querySelector('.modal-bg');
 modalBg.addEventListener('click', handleModalBgClick);
+
 // modal close 
 function handleModalBgClick(e) {
   e.stopPropagation();
@@ -13,4 +14,28 @@ function handleModalBgClick(e) {
     modal.setAttribute("style", "opacity: 0; visibility: hidden");
     modalBg.setAttribute("style", "opacity: 0; visibility: hidden");
   }
+}
+
+
+// Hover highlighting
+function mouseovered(d) {
+  node
+    .each(function (n) { n.target = n.source = false; });
+  link
+    .classed("link--target", function (l) { if (l.target === d) return l.source.source = true; })
+    .classed("link--source", function (l) { if (l.source === d) return l.target.target = true; })
+    .filter(function (l) { return l.target === d || l.source === d; })
+    .raise();
+  node
+    .classed("node--target", function (n) { return n.target; })
+    .classed("node--source", function (n) { return n.source; });
+}
+
+function mouseouted(d) {
+  link
+    .classed("link--target", false)
+    .classed("link--source", false);
+  node
+    .classed("node--target", false)
+    .classed("node--source", false);
 }
