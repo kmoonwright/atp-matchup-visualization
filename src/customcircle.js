bddfecb88c998c08252bf765358b389c028f8410
kmoonwright <kmoonwright@outlook.com>
2019-10-18 15:57:06 -0700

    Refactor circle script to run off of object type

--- src/customcircle.js
+++ src/customcircle.js
@@ -1,83 +1,126 @@
-var diameter = 960,
-  radius = diameter / 2,
-  innerRadius = radius - 120;
+// var diameter = 960,
+//   radius = diameter / 2,
+//   innerRadius = radius - 120;
 
-var cluster = d3.cluster()
-  .size([360, innerRadius]);
+// var cluster = d3.cluster()
+//   .size([360, innerRadius]);
 
-var line = d3.radialLine()
-  .curve(d3.curveBundle.beta(0.85))
-  .radius(function (d) {
-    return d.y;
-  })
-  .angle(function (d) {
-    return d.x / 180 * Math.PI;
-  });
+// var line = d3.radialLine()
+//   .curve(d3.curveBundle.beta(0.85))
+//   .radius(function (d) {
+//     return d.y;
+//   })
+//   .angle(function (d) {
+//     return d.x / 180 * Math.PI;
+//   });
 
-var svg = d3.select("#custom-circle").append("svg")
-  .attr("width", diameter)
-  .attr("height", diameter)
-  .append("g")
-  .attr("transform", "translate(" + radius + "," + radius + ")");
+// var svg = d3.select('#custom-circle').append("svg")
+//   .attr("width", diameter)
+//   .attr("height", diameter)
+//   .append("g")
+//   .attr("transform", "translate(" + radius + "," + radius + ")");
 
-var link = svg.append("g").selectAll(".link"),
-  node = svg.append("g").selectAll(".node");
+// var link = svg.append("g").selectAll(".link"),
+//   node = svg.append("g").selectAll(".node");
 
 
 // D3v5 syntax:
 // d3.csv("./data/2018_matches.csv").then(function (data, error) {
 // D3v4 syntax:
-d3.csv("https://raw.githubusercontent.com/JeffSackmann/tennis_atp/master/atp_matches_2006.csv", function (error, data) {
-  if (error) throw error;
+// let dataUrl = "./dist/data/atp_matches_1969.csv"
+const rawData = {
+  1969: "./dist/data/atp_matches_1969.csv",
+  1974: "./dist/data/atp_matches_1974.csv",
+  1984: "./dist/data/atp_matches_1984.csv",
+  2006: "./dist/data/atp_matches_2006.csv",
+  2011: "./dist/data/atp_matches_2011.csv",
+  2018: "./dist/data/atp_matches_2018.csv",
+}
 
-  // Create an object with players and their opponents
-  let playersAndOpponents = {};
-  data.forEach((datum) => {
-    if (datum.loser_name !== 'undefined' || datum.winner_name !== 'undefined') {
-      playersAndOpponents[datum.winner_name] = playersAndOpponents[datum.winner_name] || [];
-      playersAndOpponents[datum.winner_name].push(datum.loser_name);
+let link, node;
+
+function runStatRender(year) {
+  
+  d3.select("#custom-circle > svg").remove();
+
+  var diameter = 960,
+    radius = diameter / 2,
+    innerRadius = radius - 120;
+
+  var cluster = d3.cluster()
+    .size([360, innerRadius]);
+
+  var line = d3.radialLine()
+    .curve(d3.curveBundle.beta(0.85))
+    .radius(function (d) {
+      return d.y;
+    })
+    .angle(function (d) {
+      return d.x / 180 * Math.PI;
+    });
+
+  var svg = d3.select('#custom-circle').append("svg")
+    .attr("width", diameter)
+    .attr("height", diameter)
+    .append("g")
+    .attr("transform", "translate(" + radius + "," + radius + ")");
+
+  link = svg.append("g").selectAll(".link");
+  node = svg.append("g").selectAll(".node");
+
+  
+  d3.csv(rawData[year], function (error, data) {
+    if (error) throw error;
+  
+    // Create an object with players and their opponents
+    let playersAndOpponents = {};
+    data.forEach((datum) => {
+      if (datum.loser_name !== 'undefined' || datum.winner_name !== 'undefined') {
+        playersAndOpponents[datum.winner_name] = playersAndOpponents[datum.winner_name] || [];
+        playersAndOpponents[datum.winner_name].push(datum.loser_name);
+      }
+    })
+    // Parse the data
+    let importData = [];
+    for (let [key, value] of Object.entries(playersAndOpponents)) {
+      let entryObj = { "name": `${key}`, "imports": `${value}` }
+      entryObj.imports = entryObj.imports.split(',')
+      importData.push(entryObj);
     }
+  
+    var root = packageHierarchy2(importData)
+    // .sum(function(d) { return d.size; });
+  
+    cluster(root);
+  
+    node = node
+      .data(root.leaves())
+      .enter().append("text")
+      .attr("class", "node")
+      .attr("dy", "0.31em")
+      .attr("transform", function (d) {
+        return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)");
+      })
+      .attr("text-anchor", function (d) {
+        return d.x < 180 ? "start" : "end";
+      })
+      .text(function (d) {
+        return d.data.key;
+      })
+      .on("mouseover", mouseovered)
+      .on("mouseout", mouseouted);
+  
+    link = link
+      .data(packageImports2(root.leaves()))
+      .enter().append("path")
+      .each(function (d) {
+        d.source = d[0], d.target = d[d.length - 1];
+      })
+      .attr("class", "link")
+      .attr("d", line);
   })
-  // Parse the data
-  let importData = [];
-  for (let [key, value] of Object.entries(playersAndOpponents)) {
-    let entryObj = { "name": `${key}`, "imports": `${value}` }
-    entryObj.imports = entryObj.imports.split(',')
-    importData.push(entryObj);
-  }
 
-  var root = packageHierarchy2(importData)
-  // .sum(function(d) { return d.size; });
-
-  cluster(root);
-
-  node = node
-    .data(root.leaves())
-    .enter().append("text")
-    .attr("class", "node")
-    .attr("dy", "0.31em")
-    .attr("transform", function (d) {
-      return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)");
-    })
-    .attr("text-anchor", function (d) {
-      return d.x < 180 ? "start" : "end";
-    })
-    .text(function (d) {
-      return d.data.key;
-    })
-    .on("mouseover", mouseovered)
-    .on("mouseout", mouseouted);
-
-  link = link
-    .data(packageImports2(root.leaves()))
-    .enter().append("path")
-    .each(function (d) {
-      d.source = d[0], d.target = d[d.length - 1];
-    })
-    .attr("class", "link")
-    .attr("d", line);
-
-})
+}
 
 
   // Lazily construct the package hierarchy from class names.
