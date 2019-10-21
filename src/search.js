cef160d193e7f4107916943198640e0d31dd3c90
kmoonwright <kmoonwright@outlook.com>
2019-10-20 19:14:48 -0700

    Add search highlighting

--- src/search.js
+++ src/search.js
@@ -1,11 +1,29 @@
-const searchValue = document.querySelector('input').value;
-let allNodes = document.querySelectorAll('text');
-allNodes.forEach((node) => {
-  if (node.innerHTML === searchValue) {
-    node.style.backgroundColor = "red";
-  }
-})
 
 function searchNodes() {
+  const searchValue = document.querySelector('input').value;
+  let allNodes = document.querySelectorAll('text');
+  allNodes.forEach((nodeText) => {
+    if (nodeText.innerHTML.toUpperCase().includes(searchValue.toUpperCase())) {
+      // nodeText.style.backgroundColor = "FFFF00";
+      // nodeText.style.fontWeight = 700;
+      
+
+      nodeText.setAttribute('class', 'query')
   
+      setTimeout(() => {
+        nodeText.setAttribute('class', 'node')
+      }, 2000);
+      
+      // Object.assign(nodeText.style, {
+        //   fontWeight: 700,
+        
+        // })
+        
+        
+        // debugger
+        // console.log(searchValue)
+        // console.log(node.innerHTML)
+      }
+  })
+  console.log('working')
 }
