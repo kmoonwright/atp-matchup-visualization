5ebaa72758c7c0cd845300e56ff9637b36baf109
kmoonwright <kmoonwright@outlook.com>
2019-10-31 16:37:08 -0700

    Add proper modal close

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 // Modal
 const modalBg = document.querySelector('.modal-bg');
+const modal = document.querySelector('.modal');
 modalBg.addEventListener('click', handleModalBgClick);
+modal.addEventListener('click', handleModalBgClick);
 
 // modal close 
 function handleModalBgClick(e) {
   e.stopPropagation();
   let modalBg = document.querySelector(".modal-bg");
   let modal = document.querySelector('.modal');
-
-  if (
-    e.target.classList[0] === "modal-bg" ||
-    e.target.classList[0] === "modal-close__btn-single") {
-    modal.setAttribute("style", "opacity: 0; visibility: hidden");
-    modalBg.setAttribute("style", "opacity: 0; visibility: hidden");
-  }
+  modal.setAttribute("style", "opacity: 0; visibility: hidden");
+  modalBg.setAttribute("style", "opacity: 0; visibility: hidden");
+  // if (
+  //   e.target.classList[0] === "modal-bg" ||
+  //   e.target.classList[0] === "modal-close__btn-single") {
+  //   modal.setAttribute("style", "opacity: 0; visibility: hidden");
+  //   modalBg.setAttribute("style", "opacity: 0; visibility: hidden");
+  // }
   runStatRender(1969)
 }
 
